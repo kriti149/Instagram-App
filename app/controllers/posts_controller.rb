fcfbc67ef59983373381b8abf98061d0ad74276f
Kriti Gupta <kritigupta149@gmail.com>
2020-09-11 17:03:49 +0530

    Added functionality for editing and deleting the posts, resizing the images.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,40 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
 		@post = Post.new
 	end
 
+	def edit
+	end
+
 	def create
 		@post = Post.create(post_params)
-		redirect_to @post
+		redirect_to posts_path
+	end
+
+	def update
+		@post.update(post_params)
+		redirect_to posts_path
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
 	end
 
 	private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
 	def post_params
 		params.require(:post).permit({images: []}, :caption)
