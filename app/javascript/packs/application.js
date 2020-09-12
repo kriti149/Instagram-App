7b320f7c220dc165c9bac94bd9248bd61d13f827
Kriti Gupta <kritigupta149@gmail.com>
2020-09-12 12:14:17 +0530

    implemented infinite scrolling.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,19 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+$(document).ready(function() {
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+        $('.pagination').text("Please Wait...");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
